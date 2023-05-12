b4f5870b6e087bd1d2659608f6f2ff90617e0537
rukshanarupu <phar.rupu@gmail.com>
2023-05-12 17:00:04 +0600

    module-68-done

---
+++ m-68-CRUD-with-expresso-emporium-mongodb-without-wifi/Coffee-store-client/src/App.jsx
@@ -0,0 +1,32 @@
+
+import { useLoaderData } from 'react-router-dom'
+import './App.css'
+import CoffeeCard from './components/CoffeeCard';
+import { useState } from 'react';
+
+function App() {
+
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
+
+  return (
+    <div className='m-20'>
+      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: </h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees.map(coffee => 
+            // console.log(coffee)
+            <CoffeeCard
+              key={coffee._id}
+              coffee={coffee}
+              coffees={coffees}
+              setCoffees={setCoffees}
+            ></CoffeeCard>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
